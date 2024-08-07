8fc228e757c5dbaada20336ca4e0ed50a276e5fa
Filipe Pereira PC2 <filipemiguelsimoespereira@gmail.com>
2024-08-07 10:03:14 +0100

    New files

---
+++ Lists.py
@@ -0,0 +1,10 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+#add Draco name to the list
+names.append("Draco")
+
+# Change the list order
+names.sort()
+
+print (names)
--- sequences.py
+++ sequences.py
@@ -1,3 +1,9 @@
-names= ["Harry", "Ron" ,"Hermmione"]
+#names= ["Harry", "Ron" ,"Hermmione"]
 
-print (names[0])
+#print (names[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
